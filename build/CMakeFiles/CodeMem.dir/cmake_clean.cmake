a830b77bc98f6f890061eee801857d73d8f18b7e
johannes <lueke.johannes@googlemail.com>
2016-03-20 11:00:47 +0100

    add gitignore files

---
+++ build/CMakeFiles/CodeMem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CodeMem.dir/source/ConfigSettingString.cpp.o"
+  "CMakeFiles/CodeMem.dir/source/ConfigSettingInt.cpp.o"
+  "CMakeFiles/CodeMem.dir/Application.cpp.o"
+  "CodeMem.pdb"
+  "CodeMem"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CodeMem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
